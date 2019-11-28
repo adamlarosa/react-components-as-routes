002d49aa8371f1c9f77f0a0dba60adc7f98cd3d1
github username <github email address>
2019-11-27 22:58:29 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,150 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { BrowserRouter as Router, 
+//   Route, 
+//   NavLink 
+// } from 'react-router-dom';
+
+// const link = {
+//   width: '100px',
+//   padding: '12px',
+//   margin: '0 6px 6px',
+//   background: 'blue',
+//   textDecoration: 'none',
+//   color: 'white',
+// }
+
+// const Navbar = () =>
+//   <div>
+//     <NavLink
+//       to="/"
+//       /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+//       exact
+//       /* add styling to Navlink */
+//       style={link}
+//       /* add prop for activeStyle */
+//       activeStyle={{
+//         background: 'darkblue'
+//       }}
+//     >Home</NavLink>
+//         <NavLink
+//       to="/about"
+//       exact
+//       style={link}
+//       activeStyle={{
+//         background: 'darkblue'
+//       }}
+//     >About</NavLink>
+//     <NavLink
+//       to="/login"
+//       exact
+//       style={link}
+//       activeStyle={{
+//         background: 'darkblue'
+//       }}
+//     >Login</NavLink>
+//   </div>
+// ;
+
+// const Home = () => <h1>Home!</h1>;
+// const Home = () => {
+//   return (
+//     <div>
+//       <h1>Home!</h1>
+//     </div>
+//   );
+// };
+
+// const About = () => <h1>This is my about component!</h1>;
+// const About = () => {
+//   return (
+//     <div>
+//       <h1>This is my about component!</h1>
+//     </div>
+//   );
+// };
+
+// const formSumbit = (e) => {
+//   e.preventDefault();
+//   console.log(e)
+// }
+
+// const Login = () =>
+//   <form>
+//     <h1>Login</h1>
+//     <div>
+//       <input type="text" name="username" placeholder="Username" />
+//       <label htmlFor="username">Username</label>
+//     </div>
+//     <div>
+//       <input type="password" name="password" placeholder="Password" />
+//       <label htmlFor="password">Password</label>
+//     </div>
+//     <input onClick={(e) => formSumbit(e)} type="submit" value="Login" />
+//   </form>
+// ;
+// const Login = () => {
+//   return (
+//     <div>
+//       <form>
+//         <div>
+//           <input type="text" name="username" placeholder="Username" />
+//           <label htmlFor="username">Username</label>
+//         </div>
+//         <div>
+//           <input type="password" name="password" placeholder="Password" />
+//           <label htmlFor="password">Password</label>
+//         </div>
+//         <input type="submit" value="Login" />
+//       </form>
+//     </div>
+//   );
+// };
+
+// ReactDOM.render((
+//   <Router>
+//     <div>
+//       <Navbar />
+//       <Route exact path="/" component={Home} />
+//       <Route exact path="/about" component={About} />
+//       <Route exact path="/login" component={Login} />
+//     </div>
+//   </Router>),
+//   document.getElementById('root')
+// );
+// ReactDOM.render(
+//   (
+//   <Router>
+//     <div>
+//       {/* <Route path="/" render={() => <div></div> } /> */}
+      
+//       <Route exact path="/" component={Home} />
+//       <Route exact path="/about" component={About} />
+//       <Route exact path="/login" component={Login} />
+//     </div>
+//   </Router>
+//   ),
+//   document.getElementById('root')
+// );
+
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const Home = () => {
-  return (
+import Home from './home'
+import About from './about'
+import Login from './login'
+import Navbar from './navbar'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+ 
+ReactDOM.render((
+  <Router>
     <div>
-      <h1>Home!</h1>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
     </div>
-  );
-};
-
-ReactDOM.render(
-  <Home />,
+  </Router>),
   document.getElementById('root')
 );
